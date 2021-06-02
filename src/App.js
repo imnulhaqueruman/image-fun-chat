5eb973518bbb89e8c97aafa0a0ad96ce2e505ce9
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-06-03 01:29:11 +0600

    webcam setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import WebcamCapture from './WebcamCapture';
 
 function App() {
   return (
     <div className="App">
-     
+     <h4>Let's Build fun chat </h4>
+     <WebcamCapture></WebcamCapture>
     </div>
   );
 }
